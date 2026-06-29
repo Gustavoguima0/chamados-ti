1d4984c13a650fdcf1992f05c829e875cd589c9e
Gustavoguima0 <guguima12@icloud.com>
2026-06-29 10:20:56 -0300

    feat: estrutura inicial do projeto com React e Express

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <h1>Chamados TI</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+function Sidebar() {
+  return (
+    <div>
+      <h2>Menu</h2>
+      <ul>
+        <li>Chamados</li>
+        <li>Histórico</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
